3a44afec62cb260cf633509fac8f71deb7fa9e5e
EgeSythn <eseyith1@gmail.com>
2022-11-24 14:17:58 -0500

    Header added, basic skeloton, footer to be added

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,125 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+  AppShell,
+  BackgroundImage,
+  Container,
+  Header,
+  Title,
+  Text,
+  Center,
+  Box,
+} from "@mantine/core";
+import headerBackground from "./assets/headerbackground.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>
-        <a
-          href="https://www.flaticon.com/free-icons/accessibility"
-          title="accessibility icons"
+    <Container>
+      <Header
+        style={{
+          width: "120%",
+          marginTop: "-1%",
+          marginLeft: "-10%",
+          marginRight: "-10%",
+        }}
+      >
+        <BackgroundImage src={headerBackground} radius="md">
+          <Center>
+            <Title
+              order={1}
+              style={{
+                paddingTop: "5%",
+                paddingBottom: "5%",
+                paddingLeft: "10%",
+                paddingRight: "10%",
+                color: "white",
+              }}
+            >
+              Wealth & Health Inequality
+            </Title>
+          </Center>
+        </BackgroundImage>
+      </Header>
+      {/* Section 1 */}
+      <Container style={{ marginTop: "5%" }}>
+        <Title order={2} style={{ marginBottom: "2.5%" }}>
+          __TOPIC NAME__
+        </Title>
+        <Box
+          sx={(theme) => ({
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+            textAlign: "center",
+            padding: theme.spacing.xl,
+            borderRadius: theme.radius.md,
+            cursor: "pointer",
+
+            "&:hover": {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[5]
+                  : theme.colors.gray[1],
+            },
+          })}
         >
-          Accessibility icons created by Smashicons - Flaticon
-        </a>
-      </p>
-    </div>
+          <Center>__GRAPH__</Center>
+        </Box>
+      </Container>
+      {/* Section 2 */}
+      <Container style={{ marginTop: "5%" }}>
+        <Title order={2} style={{ marginBottom: "2.5%" }}>
+          __TOPIC NAME__
+        </Title>
+        <Box
+          sx={(theme) => ({
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+            textAlign: "center",
+            padding: theme.spacing.xl,
+            borderRadius: theme.radius.md,
+            cursor: "pointer",
+
+            "&:hover": {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[5]
+                  : theme.colors.gray[1],
+            },
+          })}
+        >
+          <Center>__GRAPH__</Center>
+        </Box>
+      </Container>
+      {/* Section 3 */}
+      <Container style={{ marginTop: "5%" }}>
+        <Title order={2} style={{ marginBottom: "2.5%" }}>
+          __TOPIC NAME__
+        </Title>
+        <Box
+          sx={(theme) => ({
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+            textAlign: "center",
+            padding: theme.spacing.xl,
+            borderRadius: theme.radius.md,
+            cursor: "pointer",
+
+            "&:hover": {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[5]
+                  : theme.colors.gray[1],
+            },
+          })}
+        >
+          <Center>__GRAPH__</Center>
+        </Box>
+      </Container>
+    </Container>
   );
 }
 
